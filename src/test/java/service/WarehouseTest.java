1bfe6d3a6b493a071470212aa521bfaa3467945a
AnnaLenaO <annalenao@me.com>
2024-09-16 15:54:23 +0200

    Test generics parameterized works for Category

--- src/test/java/service/WarehouseTest.java
+++ src/test/java/service/WarehouseTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
+import static entities.Category.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -18,16 +21,6 @@ class WarehouseTest {
     @MethodSource("inputProductDataProvider")
     void testCreateNewProduct(ArgumentsAccessor arguments) {
         Warehouse warehouse = new Warehouse();
-//        UUID uuidForTests = UUID.randomUUID();
-//
-//        ProductRecord newExpectedProduct = new ProductRecord(
-//                uuidForTests,
-//                arguments.getString(0),
-//                arguments.get(1, Category.class),
-//                arguments.getDouble(2),
-//                LocalDate.now(),
-//                LocalDate.now()
-//        );
 
         ProductRecord newProduct = warehouse.createNewProduct(new InputProductData(
                 arguments.getString(0),
@@ -42,7 +35,6 @@ class WarehouseTest {
         assertThat(newProduct.rating()).isEqualTo(arguments.getDouble(2));
         assertThat(newProduct.createdAt()).isNotNull();
         assertThat(newProduct.updatedAt()).isNotNull();
-
     }
 
     @ParameterizedTest(name = "Add new ProductRecord to ProductList from input arguments")
@@ -67,13 +59,66 @@ class WarehouseTest {
         assertThat(returnedProductRecord.getFirst().rating()).isEqualTo(inputProductData.rating());
         assertThat(returnedProductRecord.getFirst().createdAt()).isEqualTo(createdProductRecord.createdAt());
         assertThat(returnedProductRecord.getFirst().updatedAt()).isEqualTo(createdProductRecord.updatedAt());
-
     }
 
     static Stream<Arguments> inputProductDataProvider() {
         return Stream.of(
-                arguments("Hippolyte", Category.GALLICA, 10.0),
-                arguments("Wasagaming", Category.RUGOSA, 6.3)
+                arguments("Hippolyte", GALLICA, 10.0),
+                arguments("Wasagaming", RUGOSA, 6.3)
+        );
+    }
+
+    @ParameterizedTest(name = "Group products per generics")
+    @MethodSource("productDataProvider")
+//    void testGenericGroupingProducts(ArgumentsAccessor arguments) {
+    void testGenericGroupingProducts(Function<ProductRecord, ?> function, List<ProductRecord> productList) {
+        Warehouse warehouse = new Warehouse();
+
+        Map<?, List<ProductRecord>> groupedTestResult = warehouse.groupingProducts(function, productList);
+
+        assertThat(groupedTestResult).isNotNull();
+        assertThat(groupedTestResult.size()).isGreaterThanOrEqualTo(1);
+        Map<Category, List<ProductRecord>> groupedByCategoryTestResult = new HashMap<>();
+        groupedByCategoryTestResult.put(GALLICA, Arrays.asList(
+                new ProductRecord(UUID.fromString("bc108fc2-6785-40c4-9392-b0e93358b26e"), "Hippolyte", GALLICA, 10.0, LocalDate.parse("2024-09-16"), LocalDate.parse("2024-09-16")),
+                new ProductRecord(UUID.fromString("1a39744d-fe31-46d4-bb2f-3a62353581dc"), "Duchesse De Montebello", GALLICA, 9.8, LocalDate.parse("2024-02-10"), LocalDate.parse("2024-06-04"))
+        ));
+        groupedByCategoryTestResult.put(RUGOSA, List.of(
+                new ProductRecord(UUID.fromString("e1c63601-999c-48d8-8900-cbc4b870db2e"), "Wasagaming", RUGOSA, 6.3, LocalDate.parse("2024-09-15"), LocalDate.parse("2024-09-16"))
+        ));
+        groupedByCategoryTestResult.put(RAMBLER, Arrays.asList(
+                new ProductRecord(UUID.fromString("51feaf5d-2972-44ec-a78a-5d8a8e1be1e9"), "Lyckefund", RAMBLER, 8.7, LocalDate.parse("2024-02-10"), LocalDate.parse("2024-05-19")),
+                new ProductRecord(UUID.fromString("4ae21842-25d3-43f2-b502-626a25ac8f96"), "New Dawn", RAMBLER, 9.1, LocalDate.parse("2024-06-10"), LocalDate.parse("2024-05-19")),
+                new ProductRecord(UUID.fromString("25c20fc8-7d2c-4a16-94e3-9e797ea8472c"), "Helenae Hybrida", RAMBLER, 7.3, LocalDate.parse("2024-02-10"), LocalDate.parse("2024-07-19"))
+        ));
+        groupedByCategoryTestResult.put(CANADIAN, List.of(
+                new ProductRecord(UUID.fromString("4c00b99d-5106-4ebb-be9b-14c69b58b3a5"), "Louise Bugnet", CANADIAN, 10.0, LocalDate.parse("2024-09-01"), LocalDate.parse("2024-09-01"))
+        ));
+        groupedByCategoryTestResult.put(DAMASCENE, List.of(
+                new ProductRecord(UUID.fromString("aad34fe5-9994-42e6-baa9-e6d42340627c"), "Ispahan", DAMASCENE, 9.9, LocalDate.parse("2024-09-01"), LocalDate.parse("2024-09-04"))
+        ));
+
+        assertThat(groupedTestResult).isEqualTo(groupedByCategoryTestResult);
+
+    }
+
+    static Stream<Arguments> productDataProvider() {
+        List<ProductRecord> productListRecord = Arrays.asList(
+                new ProductRecord(UUID.fromString("bc108fc2-6785-40c4-9392-b0e93358b26e"), "Hippolyte", GALLICA, 10.0, LocalDate.parse("2024-09-16"), LocalDate.parse("2024-09-16")),
+                new ProductRecord(UUID.fromString("51feaf5d-2972-44ec-a78a-5d8a8e1be1e9"), "Lyckefund", RAMBLER, 8.7, LocalDate.parse("2024-02-10"), LocalDate.parse("2024-05-19")),
+                new ProductRecord(UUID.fromString("e1c63601-999c-48d8-8900-cbc4b870db2e"), "Wasagaming", Category.RUGOSA, 6.3, LocalDate.parse("2024-09-15"), LocalDate.parse("2024-09-16")),
+                new ProductRecord(UUID.fromString("4ae21842-25d3-43f2-b502-626a25ac8f96"), "New Dawn", RAMBLER, 9.1, LocalDate.parse("2024-06-10"), LocalDate.parse("2024-05-19")),
+                new ProductRecord(UUID.fromString("25c20fc8-7d2c-4a16-94e3-9e797ea8472c"), "Helenae Hybrida", RAMBLER, 7.3, LocalDate.parse("2024-02-10"), LocalDate.parse("2024-07-19")),
+                new ProductRecord(UUID.fromString("1a39744d-fe31-46d4-bb2f-3a62353581dc"), "Duchesse De Montebello", GALLICA, 9.8, LocalDate.parse("2024-02-10"), LocalDate.parse("2024-06-04")),
+                new ProductRecord(UUID.fromString("4c00b99d-5106-4ebb-be9b-14c69b58b3a5"), "Louise Bugnet", Category.CANADIAN, 10.0, LocalDate.parse("2024-09-01"), LocalDate.parse("2024-09-01")),
+                new ProductRecord(UUID.fromString("aad34fe5-9994-42e6-baa9-e6d42340627c"), "Ispahan", Category.DAMASCENE, 9.9, LocalDate.parse("2024-09-01"), LocalDate.parse("2024-09-04"))
+        );
+
+        return Stream.of(
+                arguments((Function<ProductRecord, UUID>) ProductRecord::id, productListRecord),
+                arguments((Function<ProductRecord, String>) ProductRecord::name, productListRecord),
+                arguments((Function<ProductRecord, Category>) ProductRecord::category, productListRecord),
+                arguments((Function<ProductRecord, LocalDate>) ProductRecord::createdAt, productListRecord)
         );
     }
 }
